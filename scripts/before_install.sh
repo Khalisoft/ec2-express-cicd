d5c19109908114830dd9e3d3c2658faac861ec9e
devkhal <pkhalisoft@gmail.com>
2022-03-31 16:39:43 +0100

    fixed shell

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,9 +5,10 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install node
 # Create working dir
 DIR="/home/ec2-user/testec2"
-if [ -d "$DIR" ]; then
-    echo "${DIR} exists"
-else
-    echo "Creating ${DIR} Directory"
-    mkdir ${DIR}
-fi
+- |
+    if [ -d "$DIR" ]; then
+        echo "${DIR} exists"
+    else
+        echo "Creating ${DIR} Directory"
+        mkdir ${DIR}
+    fi

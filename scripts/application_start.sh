ab9d9347af5fb75c399a46d8c511c2db12a0fe22
devkhal <pkhalisoft@gmail.com>
2022-03-31 16:35:09 +0100

    fixed space

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,6 @@
-
-
 cd /home/ec2-user/testec2
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh"] && \. "$NVM_DIR/bash_completion"
 
 npm install
-node app.js > app.out.log 2> app.err.log < /dev/null &
+node app.js >app.out.log 2>app.err.log </dev/null &
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,8 +3,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
-
-# Create working dir 
+# Create working dir
 DIR="/home/ec2-user/testec2"
 if [ -d "$DIR" ]; then
     echo "${DIR} exists"

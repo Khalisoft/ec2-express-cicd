5de7e7406b70e342b16b2232a6030af90c762369
devkhal <pkhalisoft@gmail.com>
2022-03-31 18:21:10 +0100

    edited scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh"] && \. "$NVM_DIR/bash_completion"
 
 npm install
-node app.js >app.out.log 2>app.err.log </dev/null &
+node app.js > app.out.log 2> app.err.log < /dev/null &
